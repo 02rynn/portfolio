5695a719e35c63420e4af97fdf1d5e7da66cd61e
o2rynn <flsgp123@naver.com>
2023-01-18 16:13:52 +0900

    first commit

---
+++ src/component/Intro.js
@@ -0,0 +1,11 @@
+
+function Intro(){
+    return(
+        <>
+        <p>여기는 인트로</p>
+        
+        </>
+    )
+}
+
+export default Intro;
---
+++ src/main/Main.js
@@ -0,0 +1,23 @@
+import {Nav, Navbar, Container} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function Main(){
+    return(
+        <>
+         <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+        여기는 메인
+        </>
+    )
+}
+
+export default Main;
